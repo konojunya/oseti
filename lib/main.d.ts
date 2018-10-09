2d3276cd1b3c1163f6ba9df0bec89b650093a5f5
konojunya <junya03dance@gmail.com>
2018-10-09 17:31:52 +0900

    add oseti.d.ts

---
+++ lib/main.d.ts
@@ -0,0 +1,4 @@
+export declare function parse(src: string): object;
+export declare function isClient(): boolean;
+export declare function get(key: string): string;
+export declare function load(filename?: string): void;
